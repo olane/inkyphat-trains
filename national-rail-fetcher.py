a1e8aa714e358654db9889416e3332aa524d71a6
Oli Lane <ojgl2@cam.ac.uk>
2018-09-11 20:38:55 +0100

    Add some bad string munging to display the next 5 trains between two stations reasonably succinctly

--- national-rail-fetcher.py
+++ national-rail-fetcher.py
@@ -3,17 +3,23 @@ import secrets
 
 darwin_sesh = DarwinLdbSession(wsdl="https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx", api_key=secrets.api_key)
 
-board = darwin_sesh.get_station_board('CMB', destination_crs='ELY')
+station_from = 'CMB'
+station_to = 'ELY'
 
-print("\nNext departures for %s" % (board.location_name))
-print("""
--------------------------------------------------------------------------------
-|  PLAT  | DEST                                        |   SCHED   |    DUE   |
-------------------------------------------------------------------------------- """)
+board = darwin_sesh.get_station_board(station_from, destination_crs=station_to)
+services = board.train_services[0:5]
+
+some_services_delayed = any(x.etd != 'On time' for x in services)
 
 # Loop through services
-for service in board.train_services:
-    print("| %6s | %43s | %9s | %8s |" %(service.platform or "", service.destination_text, service.std, service.etd))
+for service in services:
+    time_string = service.std
+    if(service.etd != 'On time'):
+        time_string += ' ({expected})'.format(expected=service.etd)
+    
+    time_string_padded = '{time: <15}'.format(time=time_string) if some_services_delayed else '{time: <5}'.format(time=time_string)
 
-# Print a footer 
-print("-------------------------------------------------------------------------------")
+    service_string = '{time} {destination: <12} '.format(time=time_string_padded, destination=service.destination_text)
+
+    print(service_string)
+
