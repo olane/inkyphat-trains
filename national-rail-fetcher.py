1c5fbfdf430f5bf8f105e55e9e46244a41eece79
Oli Lane <ojgl2@cam.ac.uk>
2018-09-11 18:49:35 +0100

    Print departure board CMB -> ELY

--- national-rail-fetcher.py
+++ national-rail-fetcher.py
@@ -3,6 +3,17 @@ import secrets
 
 darwin_sesh = DarwinLdbSession(wsdl="https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx", api_key=secrets.api_key)
 
-board = darwin_sesh.get_station_board('MAN')
+board = darwin_sesh.get_station_board('CMB', destination_crs='ELY')
 
-print(board.location_name)
+print("\nNext departures for %s" % (board.location_name))
+print("""
+-------------------------------------------------------------------------------
+|  PLAT  | DEST                                        |   SCHED   |    DUE   |
+------------------------------------------------------------------------------- """)
+
+# Loop through services
+for service in board.train_services:
+    print("| %6s | %43s | %9s | %8s |" %(service.platform or "", service.destination_text, service.std, service.etd))
+
+# Print a footer 
+print("-------------------------------------------------------------------------------")
