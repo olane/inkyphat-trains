4e33ae697135556f1118111779008cd71b4bad69
Oli Lane <ojgl2@cam.ac.uk>
2018-09-11 22:48:49 +0100

    Update the display on a loop

--- trains.py
+++ trains.py
@@ -1,33 +1,41 @@
 import inkyphat
 from PIL import ImageFont
 import national_rail_fetcher
+from time import sleep
 
 inkyphat.set_colour("yellow")
 font = ImageFont.truetype("./8bitoperator.ttf", 12)
+update_period_seconds = 3
 
-print("Getting train services")
-services = national_rail_fetcher.get_next_services()
-print("Got upcoming train services")
+def update_display():
+    print("Getting train services...")
+    services = national_rail_fetcher.get_next_services()
+    print("Got upcoming train services")
 
-some_services_delayed = any(x.etd != 'On time' for x in services)
+    some_services_delayed = any(x.etd != 'On time' for x in services)
 
-lines = []
-for service in services:
-    time_string = service.std
-    if(service.etd != 'On time'):
-        time_string += ' ({expected})'.format(expected=service.etd)
+    lines = []
+    for service in services:
+        time_string = service.std
+        if(service.etd != 'On time'):
+            time_string += ' ({expected})'.format(expected=service.etd)
+        
+        time_string_padded = '{time: <15}'.format(time=time_string) if some_services_delayed else '{time: <5}'.format(time=time_string)
+
+        service_string = '{time} {destination} '.format(time=time_string_padded, destination=service.destination_text)
+
+        lines.append(service_string)
+
+    y = 0
+    for line in lines:
+        inkyphat.text((5, y), line, inkyphat.BLACK, font)
+        y += 20
+
+    print("Displaying results...")
+    inkyphat.show()
+    print("Finished")
+
+while True:
+    update_display()
+    time.sleep(update_period_seconds)
     
-    time_string_padded = '{time: <15}'.format(time=time_string) if some_services_delayed else '{time: <5}'.format(time=time_string)
-
-    service_string = '{time} {destination} '.format(time=time_string_padded, destination=service.destination_text)
-
-    lines.append(service_string)
-
-y = 0
-for line in lines:
-    inkyphat.text((5, y), line, inkyphat.BLACK, font)
-    y += 20
-
-print("Displaying results")
-inkyphat.show()
-print("Finished")
