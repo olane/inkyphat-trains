5c9701d85ab5dba925eb7d498b165d4185e6e309
Oli Lane <ojgl2@cam.ac.uk>
2018-09-11 20:47:54 +0100

    Display train times on inkyphat

--- trains.py
+++ trains.py
@@ -1,15 +1,16 @@
 import inkyphat
 from PIL import ImageFont
+import national_rail_fetcher
 
 inkyphat.set_colour("yellow")
 
 font = ImageFont.truetype("./8bitoperator.ttf", 12)
 
-inkyphat.text((5, 0), "16:44 - Kings Lynn - 16m", inkyphat.BLACK, font)
-inkyphat.text((5, 20), "16:48 - Ely - 13m", inkyphat.BLACK, font)
-inkyphat.text((5, 40), "17:11 - Norwich - 15m (EXPECTED 17:13)", inkyphat.BLACK, font)
-inkyphat.text((5, 60), "17:44 - Kings Lynn - 16m", inkyphat.BLACK, font)
-inkyphat.text((5, 80), "17:48 - Ely - 13m", inkyphat.BLACK, font)
+lines = national_rail_fetcher.get_board_lines()
 
-
+y = 0
+for line in lines:
+    inkyphat.text((5, y), line, inkyphat.BLACK, font)
+    y += 20
+    
 inkyphat.show()
