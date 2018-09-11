adebd50f897e3cb73938dde73b06aaaaf78e6344
Oli Lane <ojgl2@cam.ac.uk>
2018-09-11 18:29:37 +0100

    Import nre darwin library and use it to print something and prove it works

---
+++ national-rail-fetcher.py
@@ -0,0 +1,8 @@
+from nredarwin.webservice import DarwinLdbSession
+import secrets
+
+darwin_sesh = DarwinLdbSession(wsdl="https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx", api_key=secrets.api_key)
+
+board = darwin_sesh.get_station_board('MAN')
+
+print(board.location_name);
