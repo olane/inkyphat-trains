a40abec4cd8d1b73fd98e86019b5d15d821b4eeb
Oli Lane <ojgl2@cam.ac.uk>
2018-09-11 22:40:58 +0100

    Refactor: extract display stuff from train fetcher (results in some duplication for now, but I expect the different renderers to diverge over time)

--- command_line_runner.py
+++ command_line_runner.py
@@ -1,6 +1,20 @@
 import national_rail_fetcher
 
-lines = national_rail_fetcher.get_board_lines()
+services = national_rail_fetcher.get_next_services()
+
+some_services_delayed = any(x.etd != 'On time' for x in services)
+
+lines = []
+for service in services:
+    time_string = service.std
+    if(service.etd != 'On time'):
+        time_string += ' ({expected})'.format(expected=service.etd)
+    
+    time_string_padded = '{time: <15}'.format(time=time_string) if some_services_delayed else '{time: <5}'.format(time=time_string)
+
+    service_string = '{time} {destination} '.format(time=time_string_padded, destination=service.destination_text)
+
+    lines.append(service_string)
 
 for line in lines:
     print(line)
--- trains.py
+++ trains.py
@@ -6,11 +6,25 @@ inkyphat.set_colour("yellow")
 
 font = ImageFont.truetype("./8bitoperator.ttf", 12)
 
-lines = national_rail_fetcher.get_board_lines()
+services = national_rail_fetcher.get_next_services()
+
+some_services_delayed = any(x.etd != 'On time' for x in services)
+
+lines = []
+for service in services:
+    time_string = service.std
+    if(service.etd != 'On time'):
+        time_string += ' ({expected})'.format(expected=service.etd)
+    
+    time_string_padded = '{time: <15}'.format(time=time_string) if some_services_delayed else '{time: <5}'.format(time=time_string)
+
+    service_string = '{time} {destination} '.format(time=time_string_padded, destination=service.destination_text)
+
+    lines.append(service_string)
 
 y = 0
 for line in lines:
     inkyphat.text((5, y), line, inkyphat.BLACK, font)
     y += 20
-    
+
 inkyphat.show()
