5c0dc0e3225a2d9d77b1470e85b6772a4587c9c6
Oli Lane <ojgl2@cam.ac.uk>
2018-09-12 08:46:57 +0100

    Move expected time to the right hand side

--- trains.py
+++ trains.py
@@ -7,26 +7,33 @@ inkyphat.set_colour("yellow")
 font = ImageFont.truetype("./8bitoperator.ttf", 12)
 update_period_seconds = 60*3
 
+def clear_area(start_x, start_y, width, height):
+    for i in range(start_x, start_x + width):
+        for j in range(start_y, start_y + height):
+            inkyphat.put_pixel((i, j), inkyphat.WHITE)
+
 def update_display():
     print("Getting train services...")
     services = national_rail_fetcher.get_next_services()
     print("Got upcoming train services")
-
-    some_services_delayed = any(x.etd != 'On time' for x in services)
 
     print("Displaying results...")
     inkyphat.clear()
     y = 0
     for service in services:
         time_string = service.std
-        if(service.etd != 'On time'):
-            time_string += ' ({expected})'.format(expected=service.etd)
-        
-        time_string_padded = '{time: <15}'.format(time=time_string) if some_services_delayed else '{time: <5}'.format(time=time_string)
+        destination_string = service.destination_text
 
-        service_string = '{time} {destination} '.format(time=time_string_padded, destination=service.destination_text)
+        time_string_padded = '{time: <5}'.format(time=time_string)
+        service_string = '{time} {destination} '.format(time=time_string_padded, destination=destination_string)
 
         inkyphat.text((5, y), service_string, inkyphat.BLACK, font)
+
+        if(service.etd != 'On time'):
+            clear_area(inkyphat.WIDTH - 40, y, 40, 20)
+            estimated_time_string = ' ({expected})'.format(expected=service.etd)
+            inkyphat.text((inkyphat.WIDTH - 40, y), estimated_time_string, inkyphat.BLACK, font)
+        
         y += 20
 
     inkyphat.show()
