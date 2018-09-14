a2751650835c0ae2ff835c058864cf464dc87778
Oli Lane <ojgl2@cam.ac.uk>
2018-09-14 23:24:49 +0100

    Make command line runner able to print time until a service

--- command_line_runner.py
+++ command_line_runner.py
@@ -3,6 +3,18 @@ import sys
 
 station_from = sys.argv[1]
 station_to = sys.argv[2]
+
+def pretty_time_delta(seconds):
+    seconds = int(seconds)
+    hours, seconds = divmod(seconds, 3600)
+    minutes, seconds = divmod(seconds, 60)
+    if hours > 0:
+        return '%dh%dm' % (hours, minutes)
+    elif minutes > 0:
+        return '%dm' % (minutes)
+    else:
+        return '%ds' % (seconds,)
+
 services = national_rail_fetcher.get_next_services(station_from, station_to)
 
 some_services_delayed = any(x.etd != 'On time' for x in services)
@@ -17,7 +29,9 @@ for service in services:
 
     service_string = '{time} {destination} '.format(time=time_string_padded, destination=service.destination_text)
 
-    lines.append(service_string)
+    time_departure_delta_string = pretty_time_delta(service.time_until_departure.total_seconds())
+    time_until_departure = ' {time}'.format(time=time_departure_delta_string)
+    lines.append(service_string + time_until_departure)
 
 for line in lines:
     print(line)
--- national_rail_fetcher.py
+++ national_rail_fetcher.py
@@ -1,5 +1,6 @@
 from nredarwin.webservice import DarwinLdbSession
 import secrets
+from datetime import datetime, time, date, timedelta
 
 darwin_sesh = DarwinLdbSession(wsdl="https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx", api_key=secrets.api_key)
 
@@ -7,4 +8,35 @@ def get_next_services(station_from, station_to):
     board = darwin_sesh.get_station_board(station_from, destination_crs=station_to)
     services = board.train_services[0:5]
 
+    for service in services:
+        attach_time_until_departure(service)
+
     return services
+
+def attach_time_until_departure(service):
+    try:
+        time_of_departure = get_time_of_departure(service)
+        datetime_of_departure = get_datetime_from_board_time(time_of_departure)
+        service.time_until_departure = datetime_of_departure - datetime.now()
+    except:
+        pass
+        # Don't care if we fail to attach time until departure
+
+# Note: this assumes that the time it is passed is
+#    - in the future
+#    - less than 24 hours in the future
+def get_datetime_from_board_time(board_time):
+    time_portion = datetime.strptime(board_time,"%H:%M")
+    d = datetime.combine(datetime.today(), time_portion.time())
+
+    if d < datetime.now():
+        # The time we parsed was past midnight, so we should combine it with tomorrow's date not today's
+        d = d + timedelta(days=1)
+
+    return d
+
+def get_time_of_departure(service):
+    time_of_departure = service.etd 
+    if(time_of_departure == 'On time'):
+        time_of_departure = service.std 
+    return time_of_departure
