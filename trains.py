1a7a1f43307e44ff485ecbd3df7f27496423af2c
Oli Lane <ojgl2@cam.ac.uk>
2018-09-15 16:54:51 +0100

    Draw the time until the service on the right hand side, below the estimated departure time if there is one

--- trains.py
+++ trains.py
@@ -11,7 +11,7 @@ inkyphat.set_colour("yellow")
 font_services = ImageFont.truetype("./8bitoperator.ttf", 11)
 font_services_offset = -4
 font_times = ImageFont.truetype("./8bitoperator.ttf", 10)
-font_times_offset = 7
+font_times_offset = -3
 
 UPDATE_PERIOD_SECONDS = 60*3
 FONT_SIZE = 11
@@ -50,17 +50,20 @@ def render_service(service, y):
     inkyphat.text((LEFT_PADDING, y + font_services_offset), time_string, inkyphat.BLACK, font_services)
     inkyphat.text((LEFT_PADDING + TIME_WIDTH, y + font_services_offset), destination_string, inkyphat.BLACK, font_services)
 
-    if(service.etd != 'On time'):
-        cutout_start = inkyphat.WIDTH - DELAY_TEXT_WIDTH
-        text_start = cutout_start + DELAY_TEXT_PADDING
+    cutout_start = inkyphat.WIDTH - DELAY_TEXT_WIDTH
+    text_start = cutout_start + DELAY_TEXT_PADDING
+    clear_area(cutout_start, y, DELAY_TEXT_WIDTH, LINE_HEIGHT)
 
-        clear_area(cutout_start, y, DELAY_TEXT_WIDTH, LINE_HEIGHT)
+    service_is_delayed = service.etd != 'On time'
+    if(service_is_delayed):
         estimated_time_string = '({expected})'.format(expected=service.etd)
         inkyphat.text((text_start, y + font_services_offset), estimated_time_string, inkyphat.BLACK, font_services)
 
     try:
+        text_offset = 10 if service_is_delayed else 0
+        text_offset = text_offset + font_times_offset
         time_departure_delta_string = pretty_time_delta(service.time_until_departure.total_seconds())
-        inkyphat.text((LEFT_PADDING+TIME_WIDTH, y + font_times_offset), time_departure_delta_string, inkyphat.YELLOW, font_times)
+        inkyphat.text((text_start, text_offset), time_departure_delta_string, inkyphat.YELLOW, font_times)
     except AttributeError:
         pass
         # there was no time until departure, presumably
